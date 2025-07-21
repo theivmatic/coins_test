307ed883bb9805f309ff4bdedbd03b7dd9d7a47c
theivmatic <theivmatic@mail.ru>
2025-07-21 21:34:13 +0500

    data entity

---
+++ lib/src/feature/coins/domain/entities/data.dart
@@ -0,0 +1,29 @@
+import 'package:coin_cap_test/src/feature/coins/data/dto/data.dart';
+
+class ResponseEntity {
+  int? timestamp;
+  List<CoinEntity>? data;
+
+  ResponseEntity({this.timestamp, this.data});
+
+  factory ResponseEntity.fromDTO(ResponseDTO dto) => ResponseEntity(
+    timestamp: dto.timestamp,
+    data: dto.data?.map(CoinEntity.fromDTO).toList(),
+  );
+}
+
+class CoinEntity {
+  String? id;
+  String? symbol;
+  String? name;
+  String? priceUsd;
+
+  CoinEntity({this.id, this.symbol, this.name, this.priceUsd});
+
+  factory CoinEntity.fromDTO(CoinDTO dto) => CoinEntity(
+    id: dto.id,
+    symbol: dto.symbol,
+    name: dto.name,
+    priceUsd: dto.priceUsd,
+  );
+}
